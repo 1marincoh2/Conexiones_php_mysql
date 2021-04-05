95de0de987f7662bc17a8bdf8d0c39ba6f667a93
1marincoh2 <jose_aries93@hotmail.com>
2021-04-05 17:05:00 -0500

    conexiones finalizadas

--- sistema.php
+++ sistema.php
@@ -4,10 +4,27 @@ require_once ("autoload.php");
 $objUsuario = new usuario();
 
 //Insertar usario
+//$insert =$objUsuario->insertUsuario("alina",9831523633,"aries@hotmail.com");
+//echo " estas conectado".$insert;
 
-$insert =$objUsuario->insertUsuario("jose",9831523633,"jose_aries@hotmail.com");
+//Actualiza registro
+//$updateUser = $objUsuario->updateUsuarios(3,"alina sayuri",963289623,"alisayu@hotmail");
+//echo $updateUser;
 
-echo " estas conectado".$insert;
 
 
+//extrae todos los registro
+$users =  $objUsuario->getUsuarios();
+print_r("<pre>");
+print_r($users);
+print_r("</pre>");
+
+//solo extrae un dato
+$userst = $objUsuario->getUsert(2);
+print_r("<pre>");
+print_r($userst);
+print_r("</pre>");
+
+$deleteUsuario = $objUsuario->deleteUser(1);
+echo $deleteUsuario;
 ?>
--- usuario.php
+++ usuario.php
@@ -31,6 +31,45 @@ class usuario extends Conexion
 
      }
 
+    public function getUsuarios()
+    {
+        $sql = "SELECT * FROM Usuario";
+        $execute = $this->conexion->query($sql);
+        $request = $execute -> fetchall(PDO::FETCH_ASSOC);
+
+        return $request;
+    }
+
+    public function updateUsuarios(int $id, string $nombre,int $telefono, string $email)
+    {
+        $this->strNombre = $nombre;
+        $this->intTelefono = $telefono;
+        $this->strEmail = $email;
+       $sql = "UPDATE Usuario SET nombre=?, telefono=?, email=? WHERE id=$id";
+       $update = $this->conexion->prepare($sql);
+       $arrData = array($this->strNombre, $this->intTelefono, $this->strEmail);
+       $resExecute = $update ->execute($arrData);
+       return $resExecute;
+    }
+  // solo trae una consulta
+    public function getUsert(int $id)
+    {
+  $sql = "SELECT * FROM Usuario WHERE id=?";
+  $arrayId = array($id);
+  $query = $this->conexion->prepare($sql);
+  $query->execute($arrayId);
+    $request = $query->fetch(PDO::FETCH_ASSOC);
+  return $request;
+    }
+
+    public function deleteUser(int $id)
+    {
+   $sql = "DELETE FROM Usuario WHERE id= ?";
+   $arrWhere =  array($id);
+   $delet = $this->conexion->prepare($sql);
+   $del = $delet->execute($arrWhere);
+   return $del;
+    }
 
 }
 
